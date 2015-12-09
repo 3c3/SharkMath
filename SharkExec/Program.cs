6e05b32533a7c3b55c9b8e30533c9cab15f532ff
3c3 <3c3code@gmail.com>
2015-12-09 19:05:51 +0200

    ProdNode вече може да се принтира и копира

--- SharkExec/Program.cs
+++ SharkExec/Program.cs
@@ -17,11 +17,14 @@ namespace SharkExec
 
             PolyNode pn1 = new PolyNode(p);
             PolyNode pn2 = new PolyNode(q);
+            PolyNode pn3 = new PolyNode(pn2);
+
+            pn3.coef.numerator = 2;
 
             Console.WriteLine(pn1.print(false, false));
             Console.WriteLine(pn2.print(false, false));
 
-            SumNode sn1 = new SumNode(pn1, pn2);
+            Node sn1 = new ProdNode(pn1, pn3);
             Console.WriteLine(sn1.print(false, false));
 
             FracNode fn1 = new FracNode(sn1, pn2);
--- SharkMath/ProdNode.cs
+++ SharkMath/ProdNode.cs
@@ -9,14 +9,52 @@ namespace SharkMath
     public class ProdNode : Node
     {
         List<Node> children;
+
+        public ProdNode()
+        {
+            children = new List<Node>();
+            coef = new Number(0);
+        }
+
+        public ProdNode(ProdNode src)
+        {
+            children = new List<Node>(src.children.Count);
+            src.children.ForEach(n => children.Add(n.copy() as Node));
+            coef = new Number(src.coef);
+        }
+
+        public ProdNode(Node n1, Node n2)
+        {
+            children = new List<Node>(2);
+            children.Add(n1.copy() as Node);
+            children.Add(n2.copy() as Node);
+            coef = new Number(1);
+        }
+        /// <summary>
+        /// Принтира произведение
+        /// </summary>
+        /// <param name="attach">Дали да се слепва, т.е. знак с интервали</param>
+        /// <param name="brackets">Игнорира се</param>
+        /// <returns></returns>
         public override string print(bool attach, bool brackets)
         {
-            throw new NotImplementedException();
+            string result = "";
+
+            if (!coef.isPosOne)
+            {
+                // при коеф. различен от 1 задължително имаме скоби
+                result += coef.print(attach, false);
+            }
+            else if (attach) result += " + "; // коефициентът е +1, така че го игнорираме
+
+            for (int i = 0; i < children.Count; i++) result += "(" + children[i].print(false, false) + ")";
+
+            return result;
         }
 
         public override object copy()
         {
-            throw new NotImplementedException();
+            return new ProdNode(this);
         }
     }
 }
